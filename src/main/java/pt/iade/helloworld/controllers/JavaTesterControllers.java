2b112234febdee927a6b1230807df45a985441e6
Tomás Bourdain da Gama <tomasbourdain@gmail.com>
2020-11-08 16:28:19 +0000

    Java  Test

--- src/main/java/pt/iade/helloworld/controllers/JavaTesterControllers.java
+++ src/main/java/pt/iade/helloworld/controllers/JavaTesterControllers.java
@@ -4,30 +4,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 @RestController
 @RequestMapping(path="/api/java/tester/")
 public class JavaTesterControllers {
-    private Logger logger = LoggerFactory.getLogger(GreeterController.class);
-    @GetMapping(path = "", produces= MediaType.APPLICATION_JSON_VALUE)
-    public String getGreeting() {
-        logger.info("Saying Hello to the world");
-        return "Hello World";
-    }
-    
-    @GetMapping(path = "{name}",
-    produces= MediaType.APPLICATION_JSON_VALUE)
-    public String getGreeting(@PathVariable("name") String name) {
-        logger.info("Saying Hello to "+name);
-        return "Hello "+name;
-    }
-
+    private Logger logger = LoggerFactory.getLogger(JavaTesterControllers.class);
+    @GetMapping(path = "/author", produces= MediaType.APPLICATION_JSON_VALUE)
+    public void getAuthor() {
+              Scanner myObj = new Scanner(System.in);
+          
+              System.out.println("Enter name, age and salary:");
+          
+              // String input
+              String name = myObj.nextLine();
+          
+              // Numerical input
+              int age = myObj.nextInt();
+              double salary = myObj.nextDouble();
+          
+              // Output input by user
+              System.out.println("Name: " + name);
+              System.out.println("Age: " + age);
+              System.out.println("Salary: " + salary);
+            }
 }
+
 
     
 /*public class Myclass{
